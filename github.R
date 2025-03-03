8bb64739ea06bb62a3ae730a22fb2ed18f3f95e8
pedroC1951 <pedroc1951@outlook.com>
2025-03-03 14:41:44 -0600

    extraer

--- github.R
+++ github.R
@@ -1,3 +1,25 @@
 variable.names <- 5
 library(usethis)
 library(gitcreds)
+names <- 6.6
+food <- 9 
+feed <- 56
+names <- 6.6
+food <- 9 
+feed <- 56
+names <- 6.6
+food <- 9 
+feed <- 56
+names <- 6.6
+food <- 9 
+feed <- 56
+names <- 6.6
+food <- 9 
+feed <- 5
+names <- 6.6
+food <- 9 
+feed <- 5
+names <- 6.6
+food <- 9 
+feed <- 56
+
